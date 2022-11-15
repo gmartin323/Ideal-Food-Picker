a7fc5d4e5f9909aa3d95ae7dc3fc14bde1bd523a
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2022-11-15 13:26:00 +0700

    Adjusted .js files

--- data.js
+++ data.js
@@ -1,3 +1,34 @@
-import { dishArray } from ".";
+export const dishArray = [
+  {
+      name: 'Herby Sandwich',
+      restaurant: 'La Studio', 
+      meal: ['breakfast', 'lunch'],
+      type: 'healthy',
+      ingredients: ['bread', 'aubergine'],
+      price: 65000,
+      closedDay: 'monday',
+      isOpen: true
+  },
+  {
+      name: 'Tempeh Bagel',
+      restaurant: 'La Studio',
+      meal: ['breakfast', 'lunch'],
+      type:'healthy',
+      ingredients: ['bread', 'tempeh'],
+      price: 65000,
+      closedDay: 'monday',
+      isOpen: true
+  },
+  {}
+]
 
-console.log(dishArray)
+const dishTemplate = {
+  name: 'string',
+  restaurant: 'string',
+  meal: ['breakfast', 'lunch', 'dinner'],
+  type: 'string',
+  ingredients: [array],
+  price: 000,
+  closedDay: 'string',
+  isOpen: true
+}
--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-export const dishArray = [
-    {
-        name: 'Herby Sandwich',
-        restaurant: 'La Studio', 
-        meal: ['breakfast', 'lunch'],
-        type: 'healthy',
-        ingredients: ['bread', 'aubergine'],
-        price: 65000,
-        closedDay: 'monday',
-        isOpen: true
-    },
-    {
-        name: 'Tempeh Bagel',
-        restaurant: 'La Studio',
-        meal: ['breakfast', 'lunch'],
-        type:'healthy',
-        ingredients: ['bread', 'tempeh'],
-        price: 65000,
-        closedDay: 'monday',
-        isOpen: true
-    },
-    {}
-]
+import { dishArray } from '.data.js';
 
-const dishTemplate = {
-    name: 'string',
-    restaurant: 'string',
-    meal: ['breakfast', 'lunch', 'dinner'],
-    type: 'string',
-    ingredients: [array],
-    price: 000,
-    closedDay: 'string',
-    isOpen: true
-}
+console.log(dishArray)
