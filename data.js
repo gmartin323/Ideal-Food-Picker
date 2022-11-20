b1c5a013456c2a7de5cb0e0027bf7269d2ddd9d5
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2022-11-20 16:24:40 +0700

    Completed databse

--- data.js
+++ data.js
@@ -19,10 +19,170 @@ export const dishArray = [
       closedDay: 'monday',
       isOpen: true
   },
-  {}
+  {
+    name: 'BBQ Pulled Mushroom Sandwich',
+    restaurant: 'Labonte',
+    meal: ['lunch'],
+    type: 'healthy',
+    ingredients: ['bread', 'mushroom'],
+    price: 80000,
+    closedDay: '',
+    isOpen: true
+  },
+  {
+    name: 'Chicken Burger',
+    restaurant: 'Kiez',
+    meal: ['lunch', 'dinner'],    
+    type: 'unhealthy',
+    ingredients: [],
+    price: 99000,
+    closedDay: 'monday',
+    isOpen: true
+  },
+  {
+    name: 'Red',
+    restaurant: 'Sol',
+    meal: ['dinner'],
+    type: 'unhealthy',
+    ingredients: [],
+    price: 200000,
+    closedDay: 'monday',
+    isOpen: true
+  },
+  {
+    name: 'Burrito',
+    restaurant: 'Anita\'s Cantina',
+    meal: ['dinner'],
+    type: 'unhealthy',
+    ingredients: [],
+    price: 160000,
+    closedDay: '',
+    isOpen: true
+  },
+  {
+    name: 'Rosemary & Garlic Fries',
+    restaurant: 'Sunkat\s',
+    meal: ['breakfast', 'lunch', 'dinner'],
+    type: 'string',
+    ingredients: [potato],
+    price: 45000,
+    closedDay: '',
+    isOpen: true
+  },
+  {
+    name: 'Poke Bowl',
+    restaurant: 'Poke Hanoi',
+    meal: ['lunch', 'dinner'],
+    type: 'healthy',
+    ingredients: ['soft tofu'],
+    price: 180000,
+    closedDay: 'sunday',
+    isOpen: true
+  },
+  {
+    name: 'Japanese Authentic Ramen',
+    restaurant: 'Vege-ro',
+    meal: ['dinner'],
+    type: 'soupy',
+    ingredients: ['noodles'],
+    price: 120000,
+    closedDay: 'monday',
+    isOpen: true
+  },
+  {
+    name: 'Bun Cha Hai Ngu',
+    restaurant: 'Thien Tra Dao',
+    meal: ['lunch', 'dinner'],
+    type: 'healthy',
+    ingredients: [],
+    price: 60000,
+    closedDay: 'sunday',
+    isOpen: true
+  },
+  {
+    name: 'Pad Thai',
+    restaurant: 'Thien Tra Dao',
+    meal: ['lunch', 'dinner'],
+    type: 'string',
+    ingredients: ['fried tofu', 'noodles'],
+    price: 60000,
+    closedDay: 'sunday',
+    isOpen: true
+  },
+  {
+    name: 'Pho Chay',
+    restaurant: 'Vegan Noodles Lo Duc',
+    meal: ['breakfast', 'lunch',],
+    type: 'soupy',
+    ingredients: [],
+    price: 30000,
+    closedDay: '',
+    isOpen: true
+  },
+  {
+    name: 'Pho Tron Chay',
+    restaurant: 'Vegan Noodles Lo Duc',
+    meal: ['breakfast', 'lunch'],
+    type: 'healthy',
+    ingredients: [],
+    price: 30000,
+    closedDay: '',
+    isOpen: true
+  },
+  {
+    name: 'Tokyo Bowl',
+    restaurant: 'iVegan',
+    meal: ['lunch', 'dinner'],
+    type: 'healthy',
+    ingredients: [],
+    price: 120000,
+    closedDay: '',
+    isOpen: true
+  },
+  {
+    name: 'Falafel Bowl',
+    restaurant: 'iVegan',
+    meal: ['lunch', 'dinner'],
+    type: 'healthy',
+    ingredients: ['falafel'],
+    price: 120000,
+    closedDay: '',
+    isOpen: true
+  },
+  {
+    name: 'Tofu Scramble',
+    restaurant: 'iVegan',
+    meal: ['breakfast', 'lunch'],
+    type: 'healthy',
+    ingredients: ['soft tofu'],
+    price: 85000,
+    closedDay: '',
+    isOpen: true
+  },
+  {
+    name: 'Đậu Phụ Sốt Cà Chua',
+    restaurant: 'La Grace',
+    meal: ['lunch', 'dinner'],
+    type: 'saucy',
+    ingredients: ['fried tofu'],
+    price: 60000,
+    closedDay: 'string',
+    isOpen: true
+  },
+  {
+    name: 'Bánh Mì Chào Nam',
+    restaurant: 'Bánh Mì 25',
+    meal: ['breakfast', 'lunch'],
+    type: 'saucy',
+    ingredients: ['mushroom'],
+    price: 59000,
+    closedDay: 'string',
+    isOpen: true
+  }
 ]
 
-const dishTemplate = {
+const dishTemplate = 
+{
   name: 'string',
   restaurant: 'string',
   meal: ['breakfast', 'lunch', 'dinner'],
